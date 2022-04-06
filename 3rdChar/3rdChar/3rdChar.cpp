4a2100341bd696445e04f25b28a6b5ced51d3ad3
M00NFACED <90569940+M00NFACED@users.noreply.github.com>
2022-04-07 01:43:59 +0300

    Add files via upload

---
+++ 3rdChar/3rdChar/3rdChar.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+    std::cin >> n;
+    char* str = new char[n];
+    std::cin >> str;
+    for (char* p = str + 2; p <= &str[n]; p += 3) {
+        std::cout << *p;
+    }
+
+
+}
